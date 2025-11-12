ea9887b10aebc7babb5b7876913fe9bd13b651f8
Lucas Rodrigues Pereira <lucas172948@gmail.com>
2025-11-12 17:04:41 +0000

    Add pagination and filtering to Accommodation API (closes #15)

--- accommodations/views.py
+++ accommodations/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
-
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Accommodation
 from .serializers import AccommodationSerializer
 
@@ -8,3 +7,7 @@ class AccommodationViewSet(viewsets.ModelViewSet):
     queryset = Accommodation.objects.all().order_by("-date_added")
     serializer_class = AccommodationSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ["location", "source", "price"]
+    search_fields = ["title", "description", "location"]
+    ordering_fields = ["price", "date_added"]
--- core/settings.py
+++ core/settings.py
@@ -72,10 +72,21 @@ DATABASES = {
 STATIC_URL = "static/"
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": (
+    "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 5,
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
+    ],
 }
+
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
